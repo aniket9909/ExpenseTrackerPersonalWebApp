60c7462fa76eb75aaec8c72bd082bfe53e0bd034
Andy <tekawadeaniket9909@gmail.com>
2022-12-15 12:06:01 +0530

    js

---
+++ index.js
@@ -0,0 +1,15 @@
+let total,expense;
+const btnexp=document.querySelector("#expbtn");
+let totalbtn = document.getElementById("depobtn")
+let remaintxt = document.getElementsByClassName("remain");
+let totaltxt = document.getElementById("total");
+let expenseamt = document.getElementById("expenseamt");
+let expreason = document.getElementById("expreason");
+let deposit = document.getElementById("deposite")
+let depoFrom = document.getElementById("from");
+let tabledepo = document.getElementById("tabledepo");
+
+btnexp.addEventListener("click",btnexpns);
+function btnexpns() {
+  alert("hii")
+}
